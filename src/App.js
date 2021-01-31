7140a2ca7df792af5f9d7c7f41178b87f0a7e0b1
SDemero <61198011+SDemero@users.noreply.github.com>
2021-01-31 00:03:48 -0500

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import FolderToggle from './component/folderToggle'
+function App() {
+  return (
+    <div className="App">
+     
+      <FolderToggle/>
+    </div>
+  );
+}
+
+export default App;
